80d7a050ce36c9b86c69fa355c781fb04b6afa7e
Cawoon <yurykavun@gmail.com>
2022-02-07 10:28:56 +0200

    Add dataset_url field to DatasetMetadata model

--- starwars_api/models.py
+++ starwars_api/models.py
@@ -10,3 +10,4 @@ def get_datasets_directory_path():
 class DatasetMetadata(models.Model):
     filepath = models.FilePathField(path=get_datasets_directory_path())
     date = models.DateTimeField(auto_now=True)
+    dataset_url = models.URLField(max_length=200, blank=True)
