885e07bf06012ef7839147a40da3cc5a1b19ca30
Cawoon <yurykavun@gmail.com>
2022-02-07 10:30:27 +0200

    Override create method of DatasetMetadataSerializer to set dataset_url based on metadata object id and save

--- starwars_api/serializers.py
+++ starwars_api/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
+from rest_framework.reverse import reverse
 from starwars_api.models import DatasetMetadata
 
 
 class DatasetMetadataSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = DatasetMetadata
-        fields = ['filepath', 'date', ]
+        fields = ['filepath', 'date', 'dataset_url', ]
+
+    def create(self, validated_data):
+        metadata = DatasetMetadata.objects.create(filepath=validated_data['filepath'])
+        metadata.dataset_url = reverse('dataset-detail', request=self.context['request'], args=[str(metadata.id)])
+        metadata.save()
+        return metadata
