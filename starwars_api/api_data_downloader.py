108dce30f09c5e1d7d5c414a38f580ad1d4a7217
Cawoon <yurykavun@gmail.com>
2022-02-08 12:58:02 +0200

    Remove a code that was delegated to transformer and saver from ApiDataDownloader

--- starwars_api/api_data_downloader.py
+++ starwars_api/api_data_downloader.py
@@ -1,34 +1,13 @@
 import requests
 import json
-import os
-import petl
-import datetime
-
-
-def transform_date(str_date):
-    date = datetime.datetime.strptime(str_date, "%Y-%m-%dT%H:%M:%S.%fZ")
-    new_format_date = date.strftime("%Y-%m-%d")
-    return new_format_date
 
 
 class ApiDataDownloader:
     api_url = 'https://swapi.dev/api/'
     homeworlds = {}
 
-    def __init__(self, file_store_directory):
-        self.filepath = self._get_filepath(file_store_directory)
-
-    def _get_filepath(self, file_store_directory):
-        existing_datasets = os.listdir(file_store_directory)
-        existing_datasets = [dataset for dataset in existing_datasets if '.csv' in dataset]
-        if existing_datasets:
-            existing_datasets_without_extension = [os.path.splitext(dataset)[0] for dataset in existing_datasets]
-            dataset_indexes = [int(dataset[-1]) for dataset in existing_datasets_without_extension]
-            last_dataset_index = max(dataset_indexes)
-        else:
-            last_dataset_index = -1
-        filepath = os.path.join(file_store_directory, 'dataset_{}.csv'.format(last_dataset_index+1))
-        return filepath
+    def __init__(self, data_saver):
+        self.data_saver = data_saver
 
     def download(self):
         next_page = None
@@ -37,11 +16,9 @@ class ApiDataDownloader:
             next_page = data['next']
             people_data = data['results']
             self._fill_homeworlds(people_data)
-            self._save(people_data)
+            self.data_saver.save(people_data)
             if not next_page:
                 break
-
-        return self.filepath
 
     def _get_people_data(self, next_page_url=None):
         if not next_page_url:
@@ -69,27 +46,5 @@ class ApiDataDownloader:
         homeworld_name = data['name']
         return homeworld_name
 
-    def _save(self, people_data):
-        people_data_table = petl.fromdicts(people_data)
-        people_data_table = self._transform_people_table(people_data_table)
 
-        if os.path.exists(self.filepath):
-            petl.appendcsv(people_data_table, self.filepath)
-        else:
-            petl.tocsv(people_data_table, self.filepath)
 
-    def _transform_people_table(self, people_data_table):
-        dates_to_drop = ['created', ]
-        references_to_drop = ['films', 'vehicles', 'starships', 'species', 'url']
-
-        people_data_table = petl.rename(people_data_table, 'edited', 'date')
-
-        people_data_table = petl.convert(people_data_table, 'date', transform_date)
-
-        for date_field in dates_to_drop:
-            people_data_table = petl.cutout(people_data_table, date_field)
-
-        for reference_field in references_to_drop:
-            people_data_table = petl.cutout(people_data_table, reference_field)
-
-        return people_data_table
