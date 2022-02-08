ccd84e07c94c2ffabb0e03050f5772b678b1e6f7
Cawoon <yurykavun@gmail.com>
2022-02-08 12:59:21 +0200

    Modify tasks to match new ApiDataDownloader interface

--- starwars_api/tasks.py
+++ starwars_api/tasks.py
@@ -1,14 +1,20 @@
 from starwars_api.models import get_datasets_directory_path
 from starwars_api.api_data_downloader import ApiDataDownloader
+from starwars_api.api_data_savers import CsvApiDataSaver
+from starwars_api.api_data_transformers import NoReferenceApiDataTransformer
 from starwars_api.serializers import DatasetMetadataSerializer
 from celery import shared_task
+
 
 @shared_task
 def download_api_character_data(data, context):
     file_store_directory = get_datasets_directory_path()
-    downloader = ApiDataDownloader(file_store_directory)
-    filepath = downloader.download()
-    data['filepath'] = filepath
+
+    transformer = NoReferenceApiDataTransformer()
+    saver = CsvApiDataSaver(transformer, file_store_directory)
+    downloader = ApiDataDownloader(saver)
+    downloader.download()
+    data['filepath'] = saver.filepath
     metadata_serializer = DatasetMetadataSerializer(data=data, context=context)
     metadata_serializer.is_valid(raise_exception=True)
     metadata_serializer.create(metadata_serializer.validated_data)
